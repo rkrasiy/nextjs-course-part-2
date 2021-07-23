b181046a2ff8ed0c94f02cda96a5c277028baafb
administrador <you@example.com>
2021-07-23 15:02:32 +0200

    add class 'active' to menu items

--- my-app/components/layout/main-header.js
+++ my-app/components/layout/main-header.js
@@ -1,24 +1,30 @@
-import Link from 'next/link';
 import classes from "./main-header.module.css"
+import { useRouter } from 'next/router';
+import LinkWrapper from "../linkwrapper/linkwrapper"
 
 function MainHeader () {
+  const router = useRouter()
+  console.log(router.pathname)
+  const active = router.pathname;
+
+  let pages = [
+    {href: "/contact", title: "Contact", active: false},
+    {href: "/events", title: "Events", active: false},
+    {href: "/furniture/kitchen", title: "Kitchens", active: false},
+    {href: "/furniture/wardrobes", title: "Wardrobes", active: false}
+  ]
+
+  let links = pages.map( link => {})
+
+
   return <header>
     <div className={classes.weblogo}>
-      <Link href="/"><a className="link-logo" title="Home Page"><img src="/logo.svg" className={classes.icon} alt="Web logo"/></a></Link>
+      <LinkWrapper href="/" className="link-logo" title="Home Page"><img src="/logo.svg" className={classes.icon} alt="Web logo"/></LinkWrapper>
     </div>
     <nav>
       <ul>
         <li>
-          <Link href="/contact">Contact</Link>
-        </li>
-        <li>
-          <Link href="/events">Events</Link>
-        </li>
-        <li>
-          <Link href="/furniture/kitchens">Kitchens</Link>
-        </li>
-        <li>
-          <Link href="/furniture/wardrobes">Wardrobes</Link>
+          <LinkWrapper href="/furniture/wardrobes" className={ active === "/furniture/wardrobes" ? "active" : ""}>Wardrobes</LinkWrapper>
         </li>
       </ul>
     </nav>
